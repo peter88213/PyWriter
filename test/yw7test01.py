16373f1a01a5e1e5537fcc0e2b92600c30d0055c
peter88213 <peter.88213@t-online.de>
2019-12-12 02:01:24 +0100

    Add test & reference data

---
+++ test/yw7test01.py
@@ -0,0 +1,16 @@
+""" Python unit tests for the yWrestler project.
+
+For further information see https://github.com/peter88213/yWrestler
+Published under the MIT License (https://opensource.org/licenses/mit-license.php)
+"""
+import os
+import unittest
+import json
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
