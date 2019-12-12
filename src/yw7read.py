ab3227372d60b6521d21262166d991d5e9d08264
peter88213 <peter.88213@t-online.de>
2019-12-12 15:24:58 +0100

    Create .odt with pypandoc

--- src/yw7read.py
+++ src/yw7read.py
@@ -3,7 +3,10 @@ Created on 12.12.2019
 
 @author: Peter
 '''
+
+import sys
 import xml.etree.ElementTree as ET
+import pypandoc
 
 
 def format_md(text):
@@ -28,15 +31,12 @@ def read_xml(yw7File):
     chapters = {}
     for chp in root.iter('CHAPTER'):
         chpNr = int(chp.find('SortOrder').text)
-        print('chapter Nr: ', chpNr)
         chpContent = ''
         chpID = chp.find('ID').text
-        print('chapter ID: ', chpID)
         chpContent = chpContent + '\\[ChID:' + chpID + '\\]\n'
         scnList = chp.find('Scenes')
         for scn in scnList.findall('ScID'):
             scnID = scn.text
-            print('sceneID: ' + scnID)
             chpContent = chpContent + '\\[ScID:' + scnID + '\\]\n'
             chpContent = chpContent + scenes[scnID] + '\n'
             chpContent = chpContent + '\\[/ScID\\]\n'
@@ -49,11 +49,21 @@ def read_xml(yw7File):
         prjText = prjText + chapters[chpNr]
         chpNr = chpNr + 1
     prjText = format_md(prjText)
-    print(prjText)
-    with open('out.md', 'w') as f:
-        f.write(prjText)
     return(prjText)
 
 
+def main():
+    """ Collect command line arguments and call the function. """
+    try:
+        yw7Path = sys.argv[1]
+        prjText = read_xml(yw7Path)
+        odtPath = yw7Path.split('.yw7')[0] + '.odt'
+        pypandoc.convert_text(
+            prjText, 'odt', format='markdown_strict', outputfile=odtPath)
+    except:
+        print('Syntax: yw7read.py filename')
+        sys.exit(1)
+
+
 if __name__ == '__main__':
-    pass
+    main()
