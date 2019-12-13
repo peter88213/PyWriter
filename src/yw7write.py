a22a3ce654f3669e37715f42d1df25240f6c0a65
peter88213 <peter.88213@t-online.de>
2019-12-13 01:55:55 +0100

    Parse yw7 raw string

--- src/yw7write.py
+++ src/yw7write.py
@@ -1,5 +1,6 @@
 """ Import proofed chapters. 
 """
+import re
 import sys
 import xml.etree.ElementTree as ET
 import pypandoc
@@ -14,20 +15,61 @@ def format_md(text):
     return(text)
 
 
+def format_yw7(text):
+    """ Convert into yw7 raw markup """
+    text = text.replace('\n\n', '\n')
+    text = text.replace('\[', '[')
+    text = text.replace('\]', ']')
+    text = re.sub('\*\*(.+?)\*\*', '[b]\g<1>[/b]', text)
+    text = re.sub('\*(.+?)\*', '[i]\g<1>[/i]', text)
+    return(text)
+
+
+def parse_yw7(rawText):
+    """ Part yw7 raw text into chapters and scenes """
+    project = {}
+    chapters = {}
+    scenes = {}
+    sceneList = []
+    sceneText = ''
+    chpID = 0
+    scnID = 0
+    projectData = rawText.split('\n')
+    for line in projectData:
+        if line.count('[ChID'):
+            chpID = re.search('[0-9]+', line).group()
+        elif line.count('[ScID'):
+            scnID = re.search('[0-9]+', line).group()
+            sceneList.append(scnID)
+        elif line.count('[/ChID]'):
+            chapters[chpID] = sceneList
+            sceneList = []
+        elif line.count('[/ScID]'):
+            scenes[scnID] = sceneText
+            sceneText = ''
+        else:
+            sceneText = sceneText + line + '\n'
+    project['scenes'] = scenes
+    project['chapters'] = chapters
+    return(project)
+
+
 def odt_to_markdown(odtFile):
     """ run pandoc to create a markdown string """
-    prjText = pypandoc.convert_file(
+    mdText = pypandoc.convert_file(
         odtFile, 'markdown_strict', format='odt', extra_args=['--wrap=none'])
-    prjText = format_md(prjText)
-    return(prjText)
+    mdText = format_md(mdText)
+    return(mdText)
 
 
-def write_markdown(prjText, mdFile):
+def write_markdown(mdText, mdFile):
     with open(mdFile, 'w') as f:
-        f.write(prjText)
+        f.write(mdText)
 
 
 def markdown_to_yw7(prjText, yw7File):
+    prjText = format_yw7(prjText)
+    prjObj = parse_yw7(prjText)
     tree = ET.parse(yw7File)
     root = tree.getroot()  # all item attributes
     return()
@@ -37,16 +79,16 @@ def main():
     """ Collect command line arguments, call conversion and generate string. """
     try:
         odtPath = sys.argv[1]
-        prjText = odt_to_markdown(odtPath)
+        mdText = odt_to_markdown(odtPath)
     except:
         print('Syntax: yw7write.py filename')
         sys.exit(1)
 
     mdPath = odtPath.split('.odt')[0] + '.md'
-    write_markdown(prjText, mdPath)
+    write_markdown(mdText, mdPath)
 
     yw7Path = odtPath.split('.odt')[0] + '.yw7'
-    markdown_to_yw7(prjText, yw7Path)
+    markdown_to_yw7(mdText, yw7Path)
 
 
 if __name__ == '__main__':
