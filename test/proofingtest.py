ac8b7fad6efbcbc657442a576b6914ba6dedb1c0
peter88213 <peter.88213@t-online.de>
2019-12-13 21:35:07 +0100

    Extended unit tests
    Proof reading is emulated by modified test data.

--- test/proofingtest.py
+++ test/proofingtest.py
@@ -20,6 +20,9 @@ TEST_DATA_PATH = 'data/'
 YW7_FILE = 'yW7 Sample Project.yw7'
 ODT_FILE = 'yW7 Sample Project.odt'
 MD_FILE = 'yW7 Sample Project.md'
+YW7_PROOFED_FILE = 'after_proofing.yw7'
+ODT_PROOFED_FILE = 'after_proofing.odt'
+MD_PROOFED_FILE = 'after_proofing.md'
 
 
 def read_file(inputFile):
@@ -37,22 +40,32 @@ def copy_file(inputFile, outputFile):
 class NormalOperation(unittest.TestCase):
     """ Test case: Normal operation
 
-        Condition: xml import is successful. 
-        Expected result: converted string matches the refData string. 
+        Condition: yw7 file is present and read/writeable. 
+        Expected result: During the whole roundtrip, the intermediate
+                    markdown file content matches 
+                    the corresponding reference string. 
     """
 
     def setUp(self):
         try:
             os.remove(TEST_EXEC_PATH + ODT_FILE)
             os.remove(TEST_EXEC_PATH + MD_FILE)
-            os.remove(TEST_EXEC_PATH + YW7_FILE)
         except:
             pass
         # Place the correct yw7 project file.
         copy_file(TEST_DATA_PATH + YW7_FILE,
                   TEST_EXEC_PATH + YW7_FILE)
 
+    def test_data(self):
+        """ Verify test data integrity. """
+        # Initial test data must differ from the "proofed" test data.
+        self.assertNotEqual(yw7.afterProofing, yw7.refData)
+        self.assertNotEqual(
+            read_file(TEST_DATA_PATH + MD_PROOFED_FILE),
+            yw7.refData)
+
     def test_proofing(self):
+        """ Complete "proof reading" roundtrip """
         prjText = yw7read.yw7_to_markdown(
             TEST_EXEC_PATH + YW7_FILE)
         # Read .yw7 file and convert xml to markdown.
@@ -75,16 +88,21 @@ class NormalOperation(unittest.TestCase):
         prjText = yw7write.read_md(TEST_EXEC_PATH + MD_FILE)
         # Read markdown from .md file.
         self.assertEqual(prjText, yw7.refData)
+
+        copy_file(TEST_DATA_PATH + MD_PROOFED_FILE,
+                  TEST_EXEC_PATH + MD_FILE)
+        # This substitutes the proof reading process.
+
+        prjText = yw7write.read_md(TEST_EXEC_PATH + MD_FILE)
+        # Read markdown from .md file.
+        self.assertEqual(prjText, yw7.afterProofing)
         yw7write.markdown_to_yw7(prjText, TEST_EXEC_PATH + YW7_FILE)
         # Convert markdown to xml and replace .yw7 file.
 
-    def tearDown(self):
-        try:
-            os.remove(TEST_EXEC_PATH + ODT_FILE)
-            os.remove(TEST_EXEC_PATH + MD_FILE)
-            os.remove(TEST_EXEC_PATH + YW7_FILE)
-        except:
-            pass
+        prjText = yw7read.yw7_to_markdown(
+            TEST_EXEC_PATH + YW7_FILE)
+        # Read .yw7 file and convert xml to markdown.
+        # self.assertEqual(prjText, yw7.afterProofing)
 
 
 def main():
