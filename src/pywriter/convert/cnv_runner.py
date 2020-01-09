5f98343ffb93ee9bbbcd27485feff1f71f045ea2
peter88213 <peter.88213@t-online.de>
2020-01-09 14:39:39 +0100

    Add a color bar to the GUI, showing success or error

--- src/pywriter/convert/cnv_runner.py
+++ src/pywriter/convert/cnv_runner.py
@@ -8,7 +8,7 @@ Published under the MIT License (https://opensource.org/licenses/mit-license.php
 """
 
 from tkinter import *
-#from tkinter import messagebox
+from tkinter import messagebox
 
 from pywriter.convert.yw7cnv import Yw7Cnv
 from pywriter.core.yw7file import Yw7File
@@ -22,14 +22,16 @@ class CnvRunner(Yw7Cnv):
     def __init__(self, sourcePath, document, extension, silentMode=True, suffix=''):
         """File conversion for proofreading """
         root = Tk()
-        root.geometry("640x480")
+        root.geometry("800x300")
         root.title(TITLE)
         self.header = Label(root, text=__doc__)
-        self.header.pack(padx=10, pady=10)
-        self.label = Label(root, text='')
-        self.label.pack(padx=10, pady=10)
-        self.messagelabel = Label(root, text='')
-        self.messagelabel.pack(padx=5, pady=5)
+        self.header.pack(padx=5, pady=5)
+        self.appInfo = Label(root, text='')
+        self.appInfo.pack(padx=5, pady=5)
+        self.successInfo = Label(root)
+        self.successInfo.pack(fill=X, expand=1, padx=50, pady=5)
+        self.processInfo = Label(root, text='')
+        self.processInfo.pack(padx=5, pady=5)
 
         self.silentMode = silentMode
 
@@ -48,31 +50,38 @@ class CnvRunner(Yw7Cnv):
             yw7Path = sourcePath
             document.filePath = sourcePath.split(
                 '.yw7')[0] + suffix + '.' + extension
-            self.label.config(
+            self.appInfo.config(
                 text='Export yWriter7 scenes content to html')
-            self.messagelabel.config(text='Project: "' + yw7Path + '"')
+            self.processInfo.config(text='Project: "' + yw7Path + '"')
 
             yw7File = Yw7File(yw7Path)
 
-            self.messagelabel.config(
+            self.processInfo.config(
                 text=self.yw7_to_document(yw7File, document))
 
         elif sourcePath.endswith(suffix + '.' + extension):
             document.filePath = sourcePath
             yw7Path = sourcePath.split(suffix + '.' + extension)[0] + '.yw7'
-            self.label.config(
+            self.appInfo.config(
                 text='Import yWriter7 scenes content from html')
-            self.messagelabel.config(
+            self.processInfo.config(
                 text='Proofed scenes in "' + document.filePath + '"')
 
             yw7File = Yw7File(yw7Path)
 
-            self.messagelabel.config(
+            self.processInfo.config(
                 text=self.document_to_yw7(document, yw7File))
 
         else:
-            self.messagelabel.config(
+            self.processInfo.config(
                 text='Argument is wrong or missing (drag and drop error?)\nInput file must be .yw7 or ' + suffix + '.' + extension + ' type.')
+            self.successInfo.config(bg='red')
+
+        if 'ERROR' in self.processInfo.cget('text'):
+            self.successInfo.config(bg='red')
+
+        elif 'SUCCESS' in self.processInfo.cget('text'):
+            self.successInfo.config(bg='green')
 
     def confirm_overwrite(self, file):
         """ Invoked by subclass if file already exists. """
