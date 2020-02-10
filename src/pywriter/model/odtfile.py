a416ec2630033e0969f6b38ce825bc16d9e6c90e
peter88213 <peter.88213@t-online.de>
2020-02-10 20:11:23 +0100

    Prepare odt conversion

---
+++ src/pywriter/model/odtfile.py
@@ -0,0 +1,406 @@
+"""OdtFile - Class for OpenDocument xml file operations and parsing.
+
+Part of the PyWriter project.
+Copyright (c) 2020 Peter Triesberger.
+For further information see https://github.com/peter88213/PyWriter
+Published under the MIT License (https://opensource.org/licenses/mit-license.php)
+"""
+
+import os
+import re
+import xml.etree.ElementTree as ET
+
+from pywriter.model.novel import Novel
+from pywriter.model.chapter import Chapter
+from pywriter.model.scene import Scene
+from pywriter.model.xform import *
+from pywriter.model.odtform import *
+
+
+class OdtFile(Novel):
+    """OpenDocument xml project file representation."""
+
+    _FILE_EXTENSION = '.odt'
+    # overwrites PywFile._FILE_EXTENSION
+
+    def __init__(self, filePath):
+        Novel.__init__(self, filePath)
+        self._cdataTags = ['Title', 'AuthorName', 'Bio', 'Desc', 'FieldTitle1', 'FieldTitle2', 'FieldTitle3', 'FieldTitle4',
+                           'LaTeXHeaderFile', 'Tags', 'AKA', 'ImageFile', 'FullName', 'Goals', 'Notes', 'RTFFile', 'SceneContent']
+        # Names of yw7 xml elements containing CDATA.
+        # ElementTree.write omits CDATA tags, so they have to be inserted
+        # afterwards.
+
+    def read(self):
+        """Parse the yw7 xml file located at filePath, fetching the Novel attributes.
+        Return a message beginning with SUCCESS or ERROR.
+        """
+
+        # Complete list of tags requiring CDATA (if incomplete).
+
+        try:
+            with open(self._filePath, 'r', encoding='utf-8') as f:
+                xmlData = f.read()
+
+        except(FileNotFoundError):
+            return 'ERROR: "' + self._filePath + '" not found.'
+
+        lines = xmlData.split('\n')
+
+        for line in lines:
+            tag = re.search('\<(.+?)\>\<\!\[CDATA', line)
+
+            if tag is not None:
+
+                if not (tag.group(1) in self._cdataTags):
+                    self._cdataTags.append(tag.group(1))
+
+        # Open the file again and let ElementTree parse its xml structure.
+
+        try:
+            self._tree = ET.parse(self._filePath)
+            root = self._tree.getroot()
+
+        except:
+            return 'ERROR: Can not process "' + self._filePath + '".'
+
+        prj = root.find('PROJECT')
+        self.title = prj.find('Title').text
+
+        if prj.find('Desc') is not None:
+            self.summary = prj.find('Desc').text
+
+        self.fieldTitle1 = prj.find('FieldTitle1').text
+        self.fieldTitle2 = prj.find('FieldTitle2').text
+        self.fieldTitle3 = prj.find('FieldTitle3').text
+        self.fieldTitle4 = prj.find('FieldTitle4').text
+
+        for chp in root.iter('CHAPTER'):
+            chId = chp.find('ID').text
+            self.chapters[chId] = Chapter()
+            self.chapters[chId].title = chp.find('Title').text
+            self.srtChapters.append(chId)
+
+            if chp.find('Desc') is not None:
+                self.chapters[chId].summary = chp.find('Desc').text
+
+            if chp.find('SectionStart') is not None:
+                self.chapters[chId].chLevel = 1
+
+            else:
+                self.chapters[chId].chLevel = 0
+
+            if chp.find('Unused') is not None:
+                self.chapters[chId].isUnused = True
+
+            else:
+                self.chapters[chId].isUnused = False
+
+            self.chapters[chId].chType = int(chp.find('Type').text)
+
+            self.chapters[chId].srtScenes = []
+
+            if chp.find('Scenes') is not None:
+
+                for scn in chp.find('Scenes').findall('ScID'):
+                    scId = scn.text
+                    self.chapters[chId].srtScenes.append(scId)
+
+        for scn in root.iter('SCENE'):
+            scId = scn.find('ID').text
+
+            self.scenes[scId] = Scene()
+            self.scenes[scId].title = scn.find('Title').text
+
+            if scn.find('Desc') is not None:
+                self.scenes[scId].summary = scn.find('Desc').text
+
+            if scn.find('Notes') is not None:
+                self.scenes[scId].sceneNotes = scn.find('Notes').text
+
+            if scn.find('Field1') is not None:
+                self.scenes[scId].field1 = scn.find('Field1').text
+
+            if scn.find('Field2') is not None:
+                self.scenes[scId].field2 = scn.find('Field2').text
+
+            if scn.find('Field3') is not None:
+                self.scenes[scId].field3 = scn.find('Field3').text
+
+            if scn.find('Field4') is not None:
+                self.scenes[scId].field4 = scn.find('Field4').text
+
+            if scn.find('Tags') is not None:
+
+                if scn.find('Tags').text is not None:
+                    self.scenes[scId].tags = scn.find('Tags').text.split(';')
+
+            if scn.find('Unused') is not None:
+                self.scenes[scId].isUnused = True
+
+            sceneContent = scn.find('SceneContent').text
+
+            if sceneContent is not None:
+                self.scenes[scId].sceneContent = sceneContent
+
+        return 'SUCCESS: ' + str(len(self.scenes)) + ' Scenes read from "' + self._filePath + '".'
+
+    def write(self, novel):
+        """Open the yw7 xml file located at filePath and replace a set 
+        of items by the novel attributes not being None.
+        Return a message beginning with SUCCESS or ERROR.
+        """
+
+        # Copy the novel's attributes to write
+
+        if novel.title is not None:
+            self.title = novel.title
+
+        if novel.summary is not None:
+            self.summary = novel.summary
+
+        if novel.fieldTitle1 is not None:
+            self.fieldTitle1 = novel.fieldTitle1
+
+        if novel.fieldTitle2 is not None:
+            self.fieldTitle2 = novel.fieldTitle2
+
+        if novel.fieldTitle3 is not None:
+            self.fieldTitle3 = novel.fieldTitle3
+
+        if novel.fieldTitle4 is not None:
+            self.fieldTitle4 = novel.fieldTitle4
+
+        '''Do not modify these items yet:
+        if novel.srtChapters != []:
+            self.srtChapters = novel.srtChapters
+        '''
+
+        if novel.scenes is not None:
+
+            for scId in novel.scenes:
+
+                if novel.scenes[scId].title is not None:
+                    self.scenes[scId].title = novel.scenes[scId].title
+
+                if novel.scenes[scId].summary is not None:
+                    self.scenes[scId].summary = novel.scenes[scId].summary
+
+                if novel.scenes[scId].sceneContent is not None:
+                    self.scenes[scId].sceneContent = novel.scenes[scId].sceneContent
+
+                if novel.scenes[scId].sceneNotes is not None:
+                    self.scenes[scId].sceneNotes = novel.scenes[scId].sceneNotes
+
+                if novel.scenes[scId].field1 is not None:
+                    self.scenes[scId].field1 = novel.scenes[scId].field1
+
+                if novel.scenes[scId].field2 is not None:
+                    self.scenes[scId].field2 = novel.scenes[scId].field2
+
+                if novel.scenes[scId].field3 is not None:
+                    self.scenes[scId].field3 = novel.scenes[scId].field3
+
+                if novel.scenes[scId].field4 is not None:
+                    self.scenes[scId].field4 = novel.scenes[scId].field4
+
+                if novel.scenes[scId].tags is not None:
+                    self.scenes[scId].tags = novel.scenes[scId].tags
+
+                '''Do not modify these items yet:
+                if novel.scenes[scId].isUnused is not None:
+                    self.scenes[scId].isUnused = novel.chapters[chId].isUnused
+                '''
+
+        if novel.chapters is not None:
+
+            for chId in novel.chapters:
+
+                if novel.chapters[chId].title is not None:
+                    self.chapters[chId].title = novel.chapters[chId].title
+
+                if novel.chapters[chId].summary is not None:
+                    self.chapters[chId].summary = novel.chapters[chId].summary
+
+                '''Do not modify these items yet:
+                if novel.chapters[chId].chLevel is not None:
+                    self.chapters[chId].chLevel = novel.chapters[chId].chLevel
+
+                if novel.chapters[chId].chType is not None:
+                    self.chapters[chId].chType = novel.chapters[chId].chType
+
+                if novel.chapters[chId].isUnused is not None:
+                    self.chapters[chId].isUnused = novel.chapters[chId].isUnused
+
+                if novel.chapters[chId].srtScenes != []:
+                    self.chapters[chId].srtScenes = novel.chapters[chId].srtScenes
+                '''
+
+        sceneCount = 0
+        root = self._tree.getroot()
+        prj = root.find('PROJECT')
+        prj.find('Title').text = self.title
+        prj.find('FieldTitle1').text = self.fieldTitle1
+        prj.find('FieldTitle2').text = self.fieldTitle2
+        prj.find('FieldTitle3').text = self.fieldTitle3
+        prj.find('FieldTitle4').text = self.fieldTitle4
+
+        if self.summary is not None:
+
+            if prj.find('Desc') is None:
+                newDesc = ET.SubElement(prj, 'Desc')
+                newDesc.text = self.summary
+
+            else:
+                prj.find('Desc').text = self.summary
+
+        for chp in root.iter('CHAPTER'):
+            chId = chp.find('ID').text
+
+            if chId in self.chapters:
+                chp.find('Title').text = self.chapters[chId].title
+
+                if self.chapters[chId].summary is not None:
+
+                    if chp.find('Desc') is None:
+                        newDesc = ET.SubElement(chp, 'Desc')
+                        newDesc.text = self.chapters[chId].summary
+
+                    else:
+                        chp.find('Desc').text = self.chapters[chId].summary
+
+                '''Do not modify these items yet:
+                chp.find('Type').text = str(self.chapters[chId].chType)
+                
+                levelInfo = chp.find('SectionStart')
+                
+                if levelInfo is not None:
+                    
+                    if self.chapters[chId].chLevel == 0:
+                         chp.remove(levelInfo)
+
+                unusedMarker = chp.find('Unused')
+                
+                if unused is not None:
+                    
+                    if not self.chapters[chId].isUnused:
+                         chp.remove(unusedMarker)
+                '''
+
+        for scn in root.iter('SCENE'):
+
+            scId = scn.find('ID').text
+
+            if scId in self.scenes:
+
+                if self.scenes[scId].title is not None:
+                    scn.find('Title').text = self.scenes[scId].title
+
+                if self.scenes[scId].summary is not None:
+
+                    if scn.find('Desc') is None:
+                        newDesc = ET.SubElement(scn, 'Desc')
+                        newDesc.text = self.scenes[scId].summary
+
+                    else:
+                        scn.find('Desc').text = self.scenes[scId].summary
+
+                if self.scenes[scId]._sceneContent is not None:
+                    scn.find(
+                        'SceneContent').text = self.scenes[scId]._sceneContent
+                    scn.find('WordCount').text = str(
+                        self.scenes[scId].wordCount)
+                    scn.find('LetterCount').text = str(
+                        self.scenes[scId].letterCount)
+
+                if self.scenes[scId].sceneNotes is not None:
+
+                    if scn.find('Notes') is None:
+                        newNotes = ET.SubElement(scn, 'Notes')
+                        newNotes.text = self.scenes[scId].sceneNotes
+
+                    else:
+                        scn.find('Notes').text = self.scenes[scId].sceneNotes
+
+                if self.scenes[scId].field1 is not None:
+
+                    if scn.find('Field1') is None:
+                        newField = ET.SubElement(scn, 'Field1')
+                        newField.text = self.scenes[scId].field1
+
+                    else:
+                        scn.find('Field1').text = self.scenes[scId].field1
+
+                if self.scenes[scId].field2 is not None:
+
+                    if scn.find('Field2') is None:
+                        newField = ET.SubElement(scn, 'Field2')
+                        newField.text = self.scenes[scId].field2
+
+                    else:
+                        scn.find('Field2').text = self.scenes[scId].field2
+
+                if self.scenes[scId].field3 is not None:
+
+                    if scn.find('Field3') is None:
+                        newField = ET.SubElement(scn, 'Field3')
+                        newField.text = self.scenes[scId].field3
+
+                    else:
+                        scn.find('Field3').text = self.scenes[scId].field3
+
+                if self.scenes[scId].field4 is not None:
+
+                    if scn.find('Field4') is None:
+                        newField = ET.SubElement(scn, 'Field4')
+                        newField.text = self.scenes[scId].field4
+
+                    else:
+                        scn.find('Field4').text = self.scenes[scId].field4
+
+                if self.scenes[scId].tags is not None:
+
+                    if scn.find('Tags') is None:
+                        newTags = ET.SubElement(scn, 'Tags')
+                        newTags.text = ';'.join(self.scenes[scId].tags)
+
+                    else:
+                        scn.find('Tags').text = ';'.join(
+                            self.scenes[scId].tags)
+
+                '''Do not modify these items yet:
+                unusedMarker = scn.find('Unused')
+                
+                if unused is not None:
+                    
+                    if not self.scenes[scId].isUnused:
+                         scn.remove(unusedMarker)
+                '''
+
+                sceneCount = sceneCount + 1
+
+        indent(root)
+        tree = ET.ElementTree(root)
+
+        try:
+            self._tree.write(self._filePath, encoding='utf-8')
+
+        except(PermissionError):
+            return 'ERROR: "' + self._filePath + '" is write protected.'
+
+        # Postprocess the xml file created by ElementTree
+        message = cdata(self._filePath, self._cdataTags)
+
+        if 'ERROR' in message:
+            return message
+
+        return 'SUCCESS: ' + str(sceneCount) + ' Scenes written to "' + self._filePath + '".'
+
+    def is_locked(self):
+        """Test whether a .lock file placed by yWriter exists.
+        """
+        if os.path.isfile(self._filePath + '.lock'):
+            return True
+
+        else:
+            return False
---
+++ src/pywriter/model/odtform.py
@@ -0,0 +1,125 @@
+"""Support HTML conversion and formatting.
+
+Part of the PyWriter project.
+Copyright (c) 2020 Peter Triesberger.
+For further information see https://github.com/peter88213/PyWriter
+Published under the MIT License (https://opensource.org/licenses/mit-license.php)
+"""
+
+import re
+
+
+SCENE_DIVIDER = '* * *'
+# To be placed between scene ending and beginning tags.
+
+ODT_HEADER = '''<?xml version="1.0" encoding="UTF-8"?>
+
+<office:document-content xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:ooo="http://openoffice.org/2004/office" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:dr3d="urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" xmlns:chart="urn:oasis:names:tc:opendocument:xmlns:chart:1.0" xmlns:rpt="http://openoffice.org/2005/report" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:number="urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0" xmlns:ooow="http://openoffice.org/2004/writer" xmlns:oooc="http://openoffice.org/2004/calc" xmlns:of="urn:oasis:names:tc:opendocument:xmlns:of:1.2" xmlns:css3t="http://www.w3.org/TR/css3-text/" xmlns:tableooo="http://openoffice.org/2009/table" xmlns:calcext="urn:org:documentfoundation:names:experimental:calc:xmlns:calcext:1.0" xmlns:drawooo="http://openoffice.org/2010/draw" xmlns:loext="urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0" xmlns:grddl="http://www.w3.org/2003/g/data-view#" xmlns:field="urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0" xmlns:math="http://www.w3.org/1998/Math/MathML" xmlns:form="urn:oasis:names:tc:opendocument:xmlns:form:1.0" xmlns:script="urn:oasis:names:tc:opendocument:xmlns:script:1.0" xmlns:dom="http://www.w3.org/2001/xml-events" xmlns:xforms="http://www.w3.org/2002/xforms" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:formx="urn:openoffice:names:experimental:ooxml-odf-interop:xmlns:form:1.0" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:officeooo="http://openoffice.org/2009/office" office:version="1.2">
+ <office:scripts/>
+ <office:font-face-decls>
+  <style:font-face style:name="StarSymbol" svg:font-family="StarSymbol" style:font-charset="x-symbol"/>
+ </office:font-face-decls>
+ <office:automatic-styles>
+  <style:style style:name="Sect1" style:family="section">
+   <style:section-properties style:editable="false">
+    <style:columns fo:column-count="1" fo:column-gap="0cm"/>
+   </style:section-properties>
+  </style:style>
+ </office:automatic-styles>
+ <office:body>
+  <office:text text:use-soft-page-breaks="true">
+   <office:forms form:automatic-focus="false" form:apply-design-mode="false"/>
+   <text:sequence-decls>
+    <text:sequence-decl text:display-outline-level="0" text:name="Illustration"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Table"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Text"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Drawing"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Figure"/>
+   </text:sequence-decls>
+'''
+
+ODT_FOOTER = '''  </office:text>
+ </office:body>
+</office:document-content>
+'''
+'''
+<text:h text:style-name="Heading_20_1" text:outline-level="1"></text:h>
+text:h text:style-name="Heading_20_2" text:outline-level="2"></text:h>
+<text:p text:style-name="Heading_20_4"></text:p>
+<text:p text:style-name="Text_20_body"></text:p>
+<text:p text:style-name="First_20_line_20_indent"></text:p>
+'''
+
+
+def to_yw7(text):
+    """Convert html tags to yw7 raw markup. Return a yw7 markup string."""
+    text = text.replace('<text:span text:style-name="Emphasis">', '[i]')
+    text = text.replace(
+        '<text:span text:style-name="Strong_20_Emphasis">', '[b]')
+    text = text.replace('</text:span>', '[/i]')
+    text = text.replace('\n', ' ')
+    text = text.replace('\r', ' ')
+    text = text.replace('\t', ' ')
+
+    text = text.replace('[/b][b]', '')
+    text = text.replace('[/i][i]', '')
+
+    while '  ' in text:
+        text = text.replace('  ', ' ')
+
+    return text
+
+
+def to_odt(text):
+    """Convert yw7 raw markup to html. Return a html string."""
+    try:
+        text = text.replace('\n', '</p>\n<p class="firstlineindent">')
+        text = text.replace('[i]', '<em>')
+        text = text.replace('[/i]', '</em>')
+        text = text.replace('[b]', '<strong>')
+        text = text.replace('[/b]', '</strong>')
+        text = re.sub('\<p(.+?)\>\<\/p\>', '<p\g<1>><br></p>', text)
+
+    except:
+        pass
+
+    return text
+
+
+def strip_markup(text):
+    """Strip yw7 raw markup. Return a plain text string."""
+    try:
+        text = text.replace('[i]', '')
+        text = text.replace('[/i]', '')
+        text = text.replace('[b]', '')
+        text = text.replace('[/b]', '')
+
+    except:
+        pass
+
+    return text
+
+
+def read_html_file(filePath):
+    """Open a html file being encoded utf-8 or ANSI.
+    Return a tuple:
+    [0] = Message beginning with SUCCESS or ERROR.
+    [1] = The file content in a single string. 
+    """
+    try:
+        with open(filePath, 'r', encoding='utf-8') as f:
+            text = (f.read())
+    except:
+        # HTML files exported by a word processor may be ANSI encoded.
+        try:
+            with open(filePath, 'r') as f:
+                text = (f.read())
+
+        except(FileNotFoundError):
+            return ('ERROR: "' + filePath + '" not found.', None)
+
+    return ('SUCCESS', text)
+
+
+if __name__ == '__main__':
+    pass
