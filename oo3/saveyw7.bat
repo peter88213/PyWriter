780c3232ae0017258e6599dd4d0dbbec5913040f
peter88213 <peter.88213@t-online.de>
2020-03-09 20:22:46 +0100

    Simplify OO3 macro; remove intermediate python script

--- oo3/saveyw7.bat
+++ oo3/saveyw7.bat
@@ -1,2 +1,1 @@
-set PYTHONPATH="c:\program files (x86)\Python38-32"
-%USERPROFILE%\AppData\Roaming\OpenOffice.org\3\user\Scripts\python\saveyw7.py %1
+%USERPROFILE%\AppData\Roaming\OpenOffice.org\3\user\Scripts\python\saveyw7.pyw %1
