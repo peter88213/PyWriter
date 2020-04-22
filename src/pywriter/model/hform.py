474959e6735a05a4745105b1b36412e835c55c5c
peter88213 <peter.88213@t-online.de>
2020-04-22 14:50:49 +0200

    Move collecrion management and book descriptions into own project

--- src/pywriter/model/hform.py
+++ src/pywriter/model/hform.py
@@ -8,24 +8,6 @@ Published under the MIT License (https://opensource.org/licenses/mit-license.php
 
 import re
 
-STYLESHEET = '<style type="text/css">\n' + \
-    'h1, h2, h3, h4, p {font: 1em monospace; margin: 3em; line-height: 1.5em}\n' + \
-    'h1, h2, h3, h4 {text-align: center}\n' +\
-    'h1 {letter-spacing: 0.2em; font-style: italic}' + \
-    'h1, h2 {font-weight: bold}\n' + \
-    'h3 {font-style: italic}\n' + \
-    'p.textbody {margin-top:0; margin-bottom:0}\n' + \
-    'p.firstlineindent {margin-top:0; margin-bottom:0; text-indent: 1em}\n' + \
-    'strong {font-weight:normal; text-transform: uppercase}\n' + \
-    '</style>\n'
-
-HTML_HEADER = '<html>\n' + '<head>\n' + \
-    '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>\n' + STYLESHEET + \
-    '<title>$bookTitle$</title>\n' + \
-    '</head>\n' + '<body>\n'
-
-HTML_FOOTER = '\n</body>\n</html>\n'
-
 
 def to_yw7(text):
     """Convert html tags to yw7 raw markup. Return a yw7 markup string."""
