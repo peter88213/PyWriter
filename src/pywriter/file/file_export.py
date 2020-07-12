ace7ad0871ed8968159240ddbaf01d63fcb3a89d
peter88213 <petert88213@gmail.com>
2020-07-12 19:53:05 +0200

    Refactor; put substitute definitions into getter methods.

--- src/pywriter/file/file_export.py
+++ src/pywriter/file/file_export.py
@@ -101,16 +101,8 @@ class FileExport(Novel):
         if novel.items is not None:
             self.items = novel.items
 
-    def write(self):
-        lines = []
-        wordsTotal = 0
-        lettersTotal = 0
-        chapterNumber = 0
-        sceneNumber = 0
-
-        # Append html header template and fill in.
-
-        projectTemplateSubst = dict(
+    def get_projectTemplateSubst(self):
+        return dict(
             Title=self.title,
             Desc=self.convert_markup(self.desc),
             AuthorName=self.author,
@@ -120,8 +112,173 @@ class FileExport(Novel):
             FieldTitle4=self.fieldTitle4,
         )
 
+    def get_chapterSubst(self, chId, chapterNumber):
+        return dict(
+            ID=chId,
+            ChapterNumber=chapterNumber,
+            Title=self.chapters[chId].title,
+            Desc=self.convert_markup(self.chapters[chId].desc),
+        )
+
+    def get_sceneSubst(self, scId, sceneNumber, wordsTotal, lettersTotal):
+
+        if self.scenes[scId].tags is not None:
+            tags = ', '.join(self.scenes[scId].tags)
+
+        else:
+            tags = ''
+
+        if self.scenes[scId].characters is not None:
+            sChList = []
+
+            for chId in self.scenes[scId].characters:
+                sChList.append(self.characters[chId].title)
+
+            sceneChars = ', '.join(sChList)
+
+            viewpointChar = sChList[0]
+
+        else:
+            sceneChars = ''
+            viewpointChar = ''
+
+        if self.scenes[scId].locations is not None:
+            sLcList = []
+
+            for lcId in self.scenes[scId].locations:
+                sLcList.append(self.locations[lcId].title)
+
+            sceneLocs = ', '.join(sLcList)
+
+        else:
+            sceneLocs = ''
+
+        if self.scenes[scId].items is not None:
+            sItList = []
+
+            for itId in self.scenes[scId].items:
+                sItList.append(self.items[itId].title)
+
+            sceneItems = ', '.join(sItList)
+
+        else:
+            sceneItems = ''
+
+        if self.scenes[scId].isReactionScene:
+            reactionScene = Scene.REACTION_MARKER
+
+        else:
+            reactionScene = Scene.ACTION_MARKER
+
+        return dict(
+            ID=scId,
+            SceneNumber=sceneNumber,
+            Title=self.scenes[scId].title,
+            Desc=self.convert_markup(self.scenes[scId].desc),
+            WordCount=str(self.scenes[scId].wordCount),
+            WordsTotal=wordsTotal,
+            LetterCount=str(self.scenes[scId].letterCount),
+            LettersTotal=lettersTotal,
+            Status=Scene.STATUS[self.scenes[scId].status],
+            SceneContent=self.convert_markup(
+                self.scenes[scId].sceneContent),
+            FieldTitle1=self.fieldTitle1,
+            FieldTitle2=self.fieldTitle2,
+            FieldTitle3=self.fieldTitle3,
+            FieldTitle4=self.fieldTitle4,
+            Field1=self.scenes[scId].field1,
+            Field2=self.scenes[scId].field2,
+            Field3=self.scenes[scId].field3,
+            Field4=self.scenes[scId].field4,
+            Date=self.scenes[scId].date,
+            Time=self.scenes[scId].time,
+            Day=self.scenes[scId].day,
+            Hour=self.scenes[scId].hour,
+            Minute=self.scenes[scId].minute,
+            LastsDays=self.scenes[scId].lastsDays,
+            LastsHours=self.scenes[scId].lastsHours,
+            LastsMinutes=self.scenes[scId].lastsMinutes,
+            ReactionScene=reactionScene,
+            Goal=self.convert_markup(self.scenes[scId].goal),
+            Conflict=self.convert_markup(self.scenes[scId].conflict),
+            Outcome=self.convert_markup(self.scenes[scId].outcome),
+            Tags=tags,
+            Characters=sceneChars,
+            Viewpoint=viewpointChar,
+            Locations=sceneLocs,
+            Items=sceneItems,
+            Notes=self.convert_markup(self.scenes[scId].sceneNotes),
+        )
+
+    def get_characterSubst(self, crId):
+
+        if self.characters[crId].tags is not None:
+            tags = ', '.join(self.characters[crId].tags)
+
+        else:
+            tags = ''
+
+        if self.characters[crId].isMajor:
+            characterStatus = Character.MAJOR_MARKER
+
+        else:
+            characterStatus = Character.MINOR_MARKER
+
+        return dict(
+            ID=crId,
+            Title=self.characters[crId].title,
+            Desc=self.convert_markup(self.characters[crId].desc),
+            Tags=tags,
+            AKA=FileExport.convert_markup(self, self.characters[crId].aka),
+            Notes=self.convert_markup(self.characters[crId].notes),
+            Bio=self.convert_markup(self.characters[crId].bio),
+            Goals=self.convert_markup(self.characters[crId].goals),
+            FullName=FileExport.convert_markup(
+                self, self.characters[crId].fullName),
+            Status=characterStatus,
+        )
+
+    def get_locationSubst(self, lcId):
+
+        if self.locations[lcId].tags is not None:
+            tags = ', '.join(self.locations[lcId].tags)
+
+        else:
+            tags = ''
+
+        return dict(
+            ID=lcId,
+            Title=self.locations[lcId].title,
+            Desc=self.convert_markup(self.locations[lcId].desc),
+            Tags=tags,
+            AKA=FileExport.convert_markup(self, self.locations[lcId].aka),
+        )
+
+    def get_itemSubst(self, itId):
+
+        if self.items[itId].tags is not None:
+            tags = ', '.join(self.items[itId].tags)
+
+        else:
+            tags = ''
+
+        return dict(
+            ID=itId,
+            Title=self.items[itId].title,
+            Desc=self.convert_markup(self.items[itId].desc),
+            Tags=tags,
+            AKA=FileExport.convert_markup(self, self.items[itId].aka),
+        )
+
+    def write(self):
+        lines = []
+        wordsTotal = 0
+        lettersTotal = 0
+        chapterNumber = 0
+        sceneNumber = 0
+
         template = Template(self.fileHeader)
-        lines.append(template.safe_substitute(projectTemplateSubst))
+        lines.append(template.safe_substitute(self.get_projectTemplateSubst()))
 
         for chId in self.srtChapters:
 
@@ -133,23 +290,14 @@ class FileExport(Novel):
 
             chapterNumber += 1
 
-            # Append chapter template and fill in.
-
-            chapterSubst = dict(
-                ID=chId,
-                ChapterNumber=chapterNumber,
-                Title=self.chapters[chId].title,
-                Desc=self.convert_markup(self.chapters[chId].desc),
-            )
-
             if self.chapters[chId].chLevel == 1 and self.partTemplate != '':
                 template = Template(self.partTemplate)
 
             else:
                 template = Template(self.chapterTemplate)
 
-            lines.append(template.safe_substitute(chapterSubst))
-
+            lines.append(template.safe_substitute(
+                self.get_chapterSubst(chId, chapterNumber)))
             firstSceneInChapter = True
 
             for scId in self.chapters[chId].srtScenes:
@@ -162,191 +310,29 @@ class FileExport(Novel):
                 if self.scenes[scId].doNotExport:
                     continue
 
-                sceneNumber += 1
-
                 if not (firstSceneInChapter or self.scenes[scId].appendToPrev):
                     lines.append(self.sceneDivider)
 
-                # Prepare data for substitution.
-
-                if self.scenes[scId].tags is not None:
-                    tags = ', '.join(self.scenes[scId].tags)
-
-                else:
-                    tags = ''
-
-                if self.scenes[scId].characters is not None:
-                    sChList = []
-
-                    for chId in self.scenes[scId].characters:
-                        sChList.append(self.characters[chId].title)
-
-                    sceneChars = ', '.join(sChList)
-
-                    viewpointChar = sChList[0]
-
-                else:
-                    sceneChars = ''
-                    viewpointChar = ''
-
-                if self.scenes[scId].locations is not None:
-                    sLcList = []
-
-                    for lcId in self.scenes[scId].locations:
-                        sLcList.append(self.locations[lcId].title)
-
-                    sceneLocs = ', '.join(sLcList)
-
-                else:
-                    sceneLocs = ''
-
-                if self.scenes[scId].items is not None:
-                    sItList = []
-
-                    for itId in self.scenes[scId].items:
-                        sItList.append(self.items[itId].title)
-
-                    sceneItems = ', '.join(sItList)
-
-                else:
-                    sceneItems = ''
-
-                if self.scenes[scId].isReactionScene:
-                    reactionScene = Scene.REACTION_MARKER
-
-                else:
-                    reactionScene = Scene.ACTION_MARKER
-
-                # Append scene template and fill in.
-
-                sceneSubst = dict(
-                    ID=scId,
-                    SceneNumber=sceneNumber,
-                    Title=self.scenes[scId].title,
-                    Desc=self.convert_markup(self.scenes[scId].desc),
-                    WordCount=str(self.scenes[scId].wordCount),
-                    WordsTotal=wordsTotal,
-                    LetterCount=str(self.scenes[scId].letterCount),
-                    LettersTotal=lettersTotal,
-                    Status=Scene.STATUS[self.scenes[scId].status],
-                    SceneContent=self.convert_markup(
-                        self.scenes[scId].sceneContent),
-                    FieldTitle1=self.fieldTitle1,
-                    FieldTitle2=self.fieldTitle2,
-                    FieldTitle3=self.fieldTitle3,
-                    FieldTitle4=self.fieldTitle4,
-                    Field1=self.scenes[scId].field1,
-                    Field2=self.scenes[scId].field2,
-                    Field3=self.scenes[scId].field3,
-                    Field4=self.scenes[scId].field4,
-                    Date=self.scenes[scId].date,
-                    Time=self.scenes[scId].time,
-                    Day=self.scenes[scId].day,
-                    Hour=self.scenes[scId].hour,
-                    Minute=self.scenes[scId].minute,
-                    LastsDays=self.scenes[scId].lastsDays,
-                    LastsHours=self.scenes[scId].lastsHours,
-                    LastsMinutes=self.scenes[scId].lastsMinutes,
-                    ReactionScene=reactionScene,
-                    Goal=self.convert_markup(self.scenes[scId].goal),
-                    Conflict=self.convert_markup(self.scenes[scId].conflict),
-                    Outcome=self.convert_markup(self.scenes[scId].outcome),
-                    Tags=tags,
-                    Characters=sceneChars,
-                    Viewpoint=viewpointChar,
-                    Locations=sceneLocs,
-                    Items=sceneItems,
-                    Notes=self.convert_markup(self.scenes[scId].sceneNotes),
-                )
+                sceneNumber += 1
                 template = Template(self.sceneTemplate)
-                lines.append(template.safe_substitute(sceneSubst))
+                lines.append(template.safe_substitute(self.get_sceneSubst(
+                    scId, sceneNumber, wordsTotal, lettersTotal)))
 
                 firstSceneInChapter = False
 
             lines.append(self.chapterEndTemplate)
 
         for crId in self.characters:
-
-            # Prepare data for substitution.
-
-            if self.characters[crId].tags is not None:
-                tags = ', '.join(self.characters[crId].tags)
-
-            else:
-                tags = ''
-
-            if self.characters[crId].isMajor:
-                characterStatus = Character.MAJOR_MARKER
-
-            else:
-                characterStatus = Character.MINOR_MARKER
-
-            # Append character template and fill in.
-
-            characterSubst = dict(
-                ID=crId,
-                Title=self.characters[crId].title,
-                Desc=self.convert_markup(self.characters[crId].desc),
-                Tags=tags,
-                AKA=FileExport.convert_markup(self, self.characters[crId].aka),
-                Notes=self.convert_markup(self.characters[crId].notes),
-                Bio=self.convert_markup(self.characters[crId].bio),
-                Goals=self.convert_markup(self.characters[crId].goals),
-                FullName=FileExport.convert_markup(
-                    self, self.characters[crId].fullName),
-                Status=characterStatus,
-            )
-
             template = Template(self.characterTemplate)
-            lines.append(template.safe_substitute(characterSubst))
+            lines.append(template.safe_substitute(self.get_characterSubst(crId)))
 
         for lcId in self.locations:
-
-            # Prepare data for substitution.
-
-            if self.locations[lcId].tags is not None:
-                tags = ', '.join(self.locations[lcId].tags)
-
-            else:
-                tags = ''
-
-            # Append location template and fill in.
-
-            locationSubst = dict(
-                ID=lcId,
-                Title=self.locations[lcId].title,
-                Desc=self.convert_markup(self.locations[lcId].desc),
-                Tags=tags,
-                AKA=FileExport.convert_markup(self, self.locations[lcId].aka),
-            )
-
             template = Template(self.locationTemplate)
-            lines.append(template.safe_substitute(locationSubst))
+            lines.append(template.safe_substitute(self.get_locationSubst(lcId)))
 
         for itId in self.items:
-
-            # Prepare data for substitution.
-
-            if self.items[itId].tags is not None:
-                tags = ', '.join(self.items[itId].tags)
-
-            else:
-                tags = ''
-
-            # Append item template and fill in.
-
-            itemSubst = dict(
-                ID=itId,
-                Title=self.items[itId].title,
-                Desc=self.convert_markup(self.items[itId].desc),
-                Tags=tags,
-                AKA=FileExport.convert_markup(self, self.items[itId].aka),
-            )
-
             template = Template(self.itemTemplate)
-            lines.append(template.safe_substitute(itemSubst))
-
-        # Append html footer and fill in.
+            lines.append(template.safe_substitute(self.get_itemSubst(itId)))
 
         lines.append(self.fileFooter)
         text = ''.join(lines)
