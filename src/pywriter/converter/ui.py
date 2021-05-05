f0c35135814881db65c785dc9d46453f2575528c
peter88213 <peter.88213@t-online.de>
2021-05-06 00:14:15 +0200

    Make minor modifications to messaging.

--- src/pywriter/converter/ui.py
+++ src/pywriter/converter/ui.py
@@ -1,6 +1,6 @@
-"""User interface for the converter: abstract facade
+"""Facade for a user interface.
 
-Copyright (c) 2020 Peter Triesberger
+Copyright (c) 2021 Peter Triesberger
 For further information see https://github.com/peter88213/PyWriter
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
@@ -10,10 +10,15 @@ class Ui():
     """Superclass for UI facades, implementing a 'silent mode'."""
 
     def __init__(self, title):
+        """Initialize text buffers for messaging.
+        """
         self.infoWhatText = ''
         self.infoHowText = ''
 
     def ask_yes_no(self, text):
+        """The application may use a subclass  
+        for confirmation requests.    
+        """
         return True
 
     def set_info_what(self, message):
