914f7c509ac7349757e1c9bea86cd6012b9ad49d
peter88213 <peter.88213@t-online.de>
2021-05-23 21:02:57 +0200

    Add a facade for a GUI featuring just message boxes.

---
+++ src/pywriter/ui/ui_mb.py
@@ -0,0 +1,32 @@
+"""Provide a facade for a GUI featuring just message boxes.
+
+Copyright (c) 2021 Peter Triesberger
+For further information see https://github.com/peter88213/PyWriter
+Published under the MIT License (https://opensource.org/licenses/mit-license.php)
+"""
+from tkinter import messagebox
+import tkinter as tk
+from pywriter.ui.ui import Ui
+
+
+class UiMb(Ui):
+    """UI subclass with messagebox."""
+
+    def __init__(self, title):
+        """Override the superclass constructor. """
+        root = tk.Tk()
+        root.withdraw()
+        self.title = title
+
+    def ask_yes_no(self, text):
+        """Override the superclass method."""
+        return messagebox.askyesno(self.title, text)
+
+    def set_info_how(self, message):
+        """Override the superclass method."""
+
+        if message.startswith('ERROR'):
+            messagebox.showerror(self.title, message)
+
+        else:
+            messagebox.showinfo(self.title, message)
