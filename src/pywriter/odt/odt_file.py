6333c7a58dbb733e807c15dfca60cc8c80de0b6e
peter88213 <peter.88213@t-online.de>
2021-06-12 21:59:16 +0200

    Prepare v3.6.6
    Fix the "proofread tags" paragraph styles.

--- src/pywriter/odt/odt_file.py
+++ src/pywriter/odt/odt_file.py
@@ -723,6 +723,18 @@ class OdtFile(OdfFile):
   <style:style style:name="List_20_Heading" style:display-name="List Heading" style:family="paragraph" style:parent-style-name="Standard" style:next-style-name="List_20_Contents" style:class="html">
    <style:paragraph-properties loext:contextual-spacing="false" fo:margin="100%" fo:margin-left="0cm" fo:margin-right="0cm" fo:margin-top="0cm" fo:margin-bottom="0cm" fo:text-indent="0cm" style:auto-text-indent="false"/>
   </style:style>
+  <style:style style:name="yWriter_20_mark" style:display-name="yWriter mark" style:family="paragraph" style:parent-style-name="Standard" style:next-style-name="Standard" style:class="text">
+   <style:text-properties fo:color="#008000" fo:font-size="10pt"/>
+  </style:style>
+  <style:style style:name="yWriter_20_mark_20_unused" style:display-name="yWriter mark unused" style:family="paragraph" style:parent-style-name="Standard" style:next-style-name="Standard" style:class="text">
+   <style:text-properties fo:color="#808080" fo:font-size="10pt"/>
+  </style:style>
+  <style:style style:name="yWriter_20_mark_20_notes" style:display-name="yWriter mark notes" style:family="paragraph" style:parent-style-name="Standard" style:next-style-name="Standard" style:class="text">
+   <style:text-properties fo:color="#0000FF" fo:font-size="10pt"/>
+  </style:style>
+  <style:style style:name="yWriter_20_mark_20_todo" style:display-name="yWriter mark todo" style:family="paragraph" style:parent-style-name="Standard" style:next-style-name="Standard" style:class="text">
+   <style:text-properties fo:color="#B22222" fo:font-size="10pt"/>
+  </style:style>
   <style:style style:name="Footnote_20_Symbol" style:display-name="Footnote Symbol" style:family="text"/>
   <style:style style:name="Page_20_Number" style:display-name="Page Number" style:family="text">
    <style:text-properties style:font-name="ITC Officina Sans Book" fo:font-size="7pt" fo:letter-spacing="0.071cm" fo:font-weight="bold"/>
