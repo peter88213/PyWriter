peter88213/PyWriter

commit 4e3fa5cd2ce0b535bfc4d91434caa409dd36337e

Refactor
- Replace all string concatenations by f-strings

src/pywriter/file/file_export.py
@@ -237,7 +237,7 @@ class FileExport(Novel):
 
             if self.scenes[scId].day is not None:
                 day = self.scenes[scId].day
-                scDate = 'Day ' + self.scenes[scId].day
+                scDate = f'Day {self.scenes[scId].day}'
 
             else:
                 day = ''
@@ -268,7 +268,7 @@ class FileExport(Novel):
                 else:
                     minute = '00'
 
-                scTime = hour.zfill(2) + ':' + minute.zfill(2)
+                scTime = f'{hour:02}:{minute:02}'
 
             else:
                 hour = ''
@@ -279,7 +279,7 @@ class FileExport(Novel):
 
         if self.scenes[scId].lastsDays is not None and self.scenes[scId].lastsDays != '0':
             lastsDays = self.scenes[scId].lastsDays
-            days = self.scenes[scId].lastsDays + 'd '
+            days = f'{self.scenes[scId].lastsDays}d '
 
         else:
             lastsDays = ''
@@ -287,7 +287,7 @@ class FileExport(Novel):
 
         if self.scenes[scId].lastsHours is not None and self.scenes[scId].lastsHours != '0':
             lastsHours = self.scenes[scId].lastsHours
-            hours = self.scenes[scId].lastsHours + 'h '
+            hours = f'{self.scenes[scId].lastsHours}h '
 
         else:
             lastsHours = ''
@@ -295,13 +295,13 @@ class FileExport(Novel):
 
         if self.scenes[scId].lastsMinutes is not None and self.scenes[scId].lastsMinutes != '0':
             lastsMinutes = self.scenes[scId].lastsMinutes
-            minutes = self.scenes[scId].lastsMinutes + 'min'
+            minutes = f'{self.scenes[scId].lastsMinutes}min'
 
         else:
             lastsMinutes = ''
             minutes = ''
 
-        duration = days + hours + minutes
+        duration = f'{days}{hours}{minutes}'
 
         sceneMapping = dict(
             ID=scId,
@@ -717,7 +717,7 @@ class FileExport(Novel):
         text = self.get_text()
 
         if os.path.isfile(self.filePath):
-            os.replace(self.filePath, self.filePath + '.bak')
+            os.replace(self.filePath, f'{self.filePath}.bak')
             backedUp = True
 
         else:
@@ -731,7 +731,7 @@ class FileExport(Novel):
         except:
 
             if backedUp:
-                os.replace(self.filePath + '.bak', self.filePath)
+                os.replace(f'{self.filePath}.bak', self.filePath)
 
             return f'ERROR: Cannot write "{os.path.normpath(self.filePath)}".'
 

src/pywriter/converter/export_target_factory.py
@@ -40,8 +40,7 @@ class ExportTargetFactory(FileFactory):
                 if suffix is None:
                     suffix = ''
 
-                targetFile = fileClass(
-                    fileName + suffix + fileClass.EXTENSION, **kwargs)
+                targetFile = fileClass(f'{fileName}{suffix}{fileClass.EXTENSION}', **kwargs)
                 return 'SUCCESS', None, targetFile
 
         return f'ERROR: File type of "{os.path.normpath(sourcePath)}" not supported.', None, None

src/pywriter/converter/import_source_factory.py
@@ -30,7 +30,7 @@ class ImportSourceFactory(FileFactory):
 
             if fileClass.SUFFIX is not None:
 
-                if sourcePath.endswith(fileClass.SUFFIX + fileClass.EXTENSION):
+                if sourcePath.endswith(f'{fileClass.SUFFIX }{fileClass.EXTENSION}'):
                     sourceFile = fileClass(sourcePath, **kwargs)
                     return 'SUCCESS', sourceFile, None
 

src/pywriter/converter/import_target_factory.py
@@ -44,9 +44,8 @@ class ImportTargetFactory(FileFactory):
 
         for fileClass in self.fileClasses:
 
-            if os.path.isfile(ywPathBasis + fileClass.EXTENSION):
-                targetFile = fileClass(
-                    ywPathBasis + fileClass.EXTENSION, **kwargs)
+            if os.path.isfile(f'{ywPathBasis}{fileClass.EXTENSION}'):
+                targetFile = fileClass(f'{ywPathBasis}{fileClass.EXTENSION}', **kwargs)
                 return 'SUCCESS', None, targetFile
 
         return 'ERROR: No yWriter project to write.', None, None

src/pywriter/converter/new_project_factory.py
@@ -43,7 +43,7 @@ class NewProjectFactory(FileFactory):
             return 'ERROR: This document is not meant to be written back.', None, None
 
         fileName, fileExtension = os.path.splitext(sourcePath)
-        targetFile = Yw7File(fileName + Yw7File.EXTENSION, **kwargs)
+        targetFile = Yw7File(f'{fileName}{Yw7File.EXTENSION}', **kwargs)
 
         if sourcePath.endswith('.html'):
 
@@ -69,7 +69,7 @@ class NewProjectFactory(FileFactory):
 
                 if fileClass.SUFFIX is not None:
 
-                    if sourcePath.endswith(fileClass.SUFFIX + fileClass.EXTENSION):
+                    if sourcePath.endswith(f'{fileClass.SUFFIX}{fileClass.EXTENSION}'):
                         sourceFile = fileClass(sourcePath, **kwargs)
                         return 'SUCCESS', sourceFile, targetFile
 

src/pywriter/converter/yw_cnv_ui.py
@@ -162,7 +162,7 @@ class YwCnvUi(YwCnv):
 
     def confirm_overwrite(self, filePath):
         """Return boolean permission to overwrite the target file, overriding the superclass method."""
-        return self.ui.ask_yes_no('Overwrite existing file "{os.path.normpath(filePath)}"?')
+        return self.ui.ask_yes_no(f'Overwrite existing file "{os.path.normpath(filePath)}"?')
 
     def delete_tempfile(self, filePath):
         """Delete filePath if it is a temporary file no longer needed."""

src/pywriter/html/html_proof.py
@@ -88,4 +88,4 @@ class HtmlProof(HtmlFile):
         Overwrites HTMLparser.handle_data().
         """
         if self._prefix is not None:
-            self._lines.append(self._prefix + data)
+            self._lines.append(f'{self._prefix}{data}')

src/pywriter/model/novel.py
@@ -166,12 +166,11 @@ class Novel():
         else:
             suffix = ''
 
-        if filePath.lower().endswith((suffix + self.EXTENSION).lower()):
+        if filePath.lower().endswith(f'{suffix}{self.EXTENSION}'.lower()):
             self._filePath = filePath
             head, tail = os.path.split(os.path.realpath(filePath))
             self.projectPath = quote(head.replace('\\', '/'), '/:')
-            self.projectName = quote(tail.replace(
-                suffix + self.EXTENSION, ''))
+            self.projectName = quote(tail.replace(f'{suffix}{self.EXTENSION}', ''))
 
     def convert_to_yw(self, text):
         """Return text, converted from source format to yw7 markup.

commit 5da5a5a423fe3a86546fb2b19d139c54c04d7605

Prepare v4.1.8
- Use the "pack" geometry manager instead of "grid" in UiTk.
- Fix a regression from  4e3fa5c where strings are mistakenly treated as
integers in FileExport.

src/pywriter/file/file_export.py
@@ -269,7 +269,7 @@ class FileExport(Novel):
                 else:
                     minute = '00'
 
-                scTime = f'{hour:02}:{minute:02}'
+                scTime = f'{hour.zfill(2)}:{minute.zfill(2)}'
 
             else:
                 hour = ''

src/pywriter/ui/ui_tk.py
@@ -19,20 +19,22 @@ class UiTk(Ui):
         super().__init__(title)
 
         self.root = Tk()
+        self.root.minsize(400, 150)
+        self.root.resizable(width=FALSE, height=FALSE)
         self.root.title(title)
 
         self.rowCount = 1
         self.appInfo = Label(self.root, text='')
-        self.appInfo.grid(row=self.rowCount, column=1, padx=5, pady=5)
+        self.appInfo.pack(padx=20, pady=5)
 
         self.rowCount += 1
-        self.processInfo = Label(self.root, text='')
-        self.processInfo.grid(row=self.rowCount, column=1, pady=10, padx=10)
+        self.processInfo = Label(self.root, text='', padx=20)
+        self.processInfo.pack(pady=20, fill='both')
 
         self.rowCount += 1
         self.root.quitButton = Button(text="Quit", command=quit)
         self.root.quitButton.config(height=1, width=10)
-        self.root.quitButton.grid(row=self.rowCount, column=1, pady=10)
+        self.root.quitButton.pack(pady=10)
 
     def ask_yes_no(self, text):
         """Override the Ui method."""
@@ -68,4 +70,4 @@ class UiTk(Ui):
         self.root.openButton = Button(text="Open", command=open_cmd)
         self.root.openButton.config(height=1, width=10)
         self.rowCount += 1
-        self.root.openButton.grid(row=self.rowCount, column=1, pady=10)
+        self.root.openButton.pack(pady=10)