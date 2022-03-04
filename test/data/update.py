0f878755b9ac8b3e424f3455ea3cf1db6afd9d58
peter88213 <peter.88213@t-online.de>
2022-03-04 22:10:51 +0100

    #18 Update test data

--- test/data/update.py
+++ test/data/update.py
@@ -6,11 +6,15 @@ with open(filePath, 'r', encoding='utf-8') as f:
     text = f.read()
     
 old = '''
- <office:automatic-styles />
+  <style:font-face style:name="Arial" svg:font-family="Arial" style:font-family-generic="swiss" style:font-pitch="variable"/>
+  <style:font-face style:name="Segoe UI" svg:font-family="&apos;Segoe UI&apos;" style:font-adornments="Standard" style:font-family-generic="swiss" style:font-pitch="variable"/>
+  <style:font-face style:name="Arial Unicode MS" svg:font-family="&apos;Arial Unicode MS&apos;" style:font-family-generic="system" style:font-pitch="variable"/>
+  <style:font-face style:name="Microsoft YaHei" svg:font-family="&apos;Microsoft YaHei&apos;" style:font-family-generic="system" style:font-pitch="variable"/>
+  <style:font-face style:name="Tahoma" svg:font-family="Tahoma" style:font-family-generic="system" style:font-pitch="variable"/>
 '''
     
 new = '''
- <office:automatic-styles/>
+  <style:font-face style:name="Segoe UI" svg:font-family="&apos;Segoe UI&apos;" style:font-adornments="Standard" style:font-family-generic="swiss" style:font-pitch="variable"/>
 '''    
 
 if old in text: 
