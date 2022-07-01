85bf52c06f712e4c41ffa5a9e6c0a2174df4653f
peter88213 <peter.88213@t-online.de>
2022-07-01 21:28:22 +0200

    Prepare v5.12.4
    Fix word counting considering ellipses.

--- src/pywriter/model/scene.py
+++ src/pywriter/model/scene.py
@@ -222,7 +222,7 @@ class Scene:
     def sceneContent(self, text):
         """Set sceneContent updating word count and letter count."""
         self._sceneContent = text
-        text = re.sub('--|—|–', ' ', text)
+        text = re.sub('--|—|–|…', ' ', text)
         # Make dashes separate words
         text = re.sub('\[.+?\]|\/\*.+?\*\/|\.|\,|-', '', text)
         # Remove comments and yWriter raw markup for word count; make hyphens join words
