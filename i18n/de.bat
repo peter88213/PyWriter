06740764ac9d2118832c38244e59e7529229a159
peter88213 <peter.88213@t-online.de>
2022-08-09 11:50:18 +0200

    Move localization tools.

---
+++ i18n/de.bat
@@ -0,0 +1,2 @@
+.\msgfmt.py -o locale\de\LC_MESSAGES\pywriter.mo ..\i18n\de.po
+xcopy /s /y locale\ ..\src\sample\locale\
--- tools/de.bat
+++
@@ -1,2 +0,0 @@
-.\msgfmt.py -o ..\i18n\locale\de\LC_MESSAGES\pywriter.mo ..\i18n\de.po
-xcopy /s /y ..\i18n\locale\ ..\src\sample\locale\
