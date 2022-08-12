65247268253d16974d66823fcd19ddc52e2090e7
peter88213 <peter.88213@t-online.de>
2022-08-12 22:00:46 +0200

    Prepare v5.22.2
    OdtCharacters: Translate subheadings.

--- src/pywriter/odt/odt_characters.py
+++ src/pywriter/odt/odt_characters.py
@@ -20,21 +20,21 @@ class OdtCharacters(OdtFile):
 <text:p text:style-name="Subtitle">$AuthorName</text:p>
 '''
 
-    _characterTemplate = '''<text:h text:style-name="Heading_20_2" text:outline-level="2">$Title$FullName$AKA</text:h>
+    _characterTemplate = f'''<text:h text:style-name="Heading_20_2" text:outline-level="2">$Title$FullName$AKA</text:h>
 <text:section text:style-name="Sect1" text:name="CrID:$ID">
-<text:h text:style-name="Heading_20_3" text:outline-level="3">Description</text:h>
+<text:h text:style-name="Heading_20_3" text:outline-level="3">{_("Description")}</text:h>
 <text:section text:style-name="Sect1" text:name="CrID_desc:$ID">
 <text:p text:style-name="Text_20_body">$Desc</text:p>
 </text:section>
-<text:h text:style-name="Heading_20_3" text:outline-level="3">Bio</text:h>
+<text:h text:style-name="Heading_20_3" text:outline-level="3">{_("Bio")}</text:h>
 <text:section text:style-name="Sect1" text:name="CrID_bio:$ID">
 <text:p text:style-name="Text_20_body">$Bio</text:p>
 </text:section>
-<text:h text:style-name="Heading_20_3" text:outline-level="3">Goals</text:h>
+<text:h text:style-name="Heading_20_3" text:outline-level="3">{_("Goals")}</text:h>
 <text:section text:style-name="Sect1" text:name="CrID_goals:$ID">
 <text:p text:style-name="Text_20_body">$Goals</text:p>
 </text:section>
-<text:h text:style-name="Heading_20_3" text:outline-level="3">Notes</text:h>
+<text:h text:style-name="Heading_20_3" text:outline-level="3">{_("Notes")}</text:h>
 <text:section text:style-name="Sect1" text:name="CrID_notes:$ID">
 <text:p text:style-name="Text_20_body">$Notes</text:p>
 </text:section>
