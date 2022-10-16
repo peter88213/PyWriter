9402fad52471d195188dd29aedfd0b1807444c87
peter88213 <peter.88213@t-online.de>
2022-10-16 12:13:01 +0200

    Prepare v7.13.0
    - Add the yw7_importer module.

--- src/pywriter/converter/__init__.py
+++ src/pywriter/converter/__init__.py
@@ -2,17 +2,18 @@
 
 Modules:
 
-yw_cnv -- Provide the base class for Novel file conversion.
-yw_cnv_ui -- Provide a class for Novel file conversion with user interface.
-yw_cnv_ff -- Provide a class for Novel file conversion with file factories.
-file_factory -- Provide a base class for factories that instantiate conversion objects.
 export_source_factory -- Provide a factory class for any export source object.
 export_target_factory -- Provide a factory class for any export target object.
+file_factory -- Provide a base class for factories that instantiate conversion objects.
 import_source_factory -- Provide a factory class for any import source object.
 import_target_factory -- Provide a factory class for any import target object.
 new_project_factory -- Provide a factory class for source and target objects to create a new yWriter project.
 yw7_converter -- Provide a converter class for yWriter 7 universal import and export.
 yw7_exporter -- Provide a converter class for universal export from a yWriter 7 project.
+yw7_importer -- Provide a converter class for yWriter 7 universal import.
+yw_cnv -- Provide the base class for Novel file conversion.
+yw_cnv_ff -- Provide a class for Novel file conversion with file factories.
+yw_cnv_ui -- Provide a class for Novel file conversion with user interface.
 
 Copyright (c) 2022 Peter Triesberger
 For further information see https://github.com/peter88213/PyWriter
