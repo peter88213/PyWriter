608479bae6099f70b807588fcd82bc44b6350d17
peter88213 <peter.88213@t-online.de>
2023-06-09 09:33:39 +0200

    Update docstrings

--- src/pywriter/test/helper.py
+++ src/pywriter/test/helper.py
@@ -1,8 +1,10 @@
 """Helper module for reading utf-8 or ANSI encoded files.
 
+Copyright (c) 2023 Peter Triesberger
 For further information see https://github.com/peter88213/PyWriter
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
+
 
 def read_file(inputFile):
     """Read a utf-8 or ANSI encoded text file.
--- src/pywriter/test/import_test.py
+++ src/pywriter/test/import_test.py
@@ -2,6 +2,7 @@
 
 Import standard test routines used for Regression test.
 
+Copyright (c) 2023 Peter Triesberger
 For further information see https://github.com/peter88213/PyWriter
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
